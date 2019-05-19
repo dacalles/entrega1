4283c730b383b3bdbd1f0b3f4542f7f8455bc0f3
dacalles <dacalles@unal.edu.co>
2019-05-18 22:54:51 -0500

    Primer punto

--- Entrega1.js
+++ Entrega1.js
@@ -1,18 +1,53 @@
-let estudiante = {
-    nombre: 'juan',
-    edad: 25,
-    notas: {
-        matematicas: 3,
-        ingles: 4,
-        programacion: 5
+let cursos = [{
+    id: 0,
+    nombre: 'calculo',
+    duracion: 6,
+    valor: 300000
+    },
+    {
+        id: 1,
+        nombre: 'ingles',
+        duracion: 5,
+        valor: 250000
+    },
+    {
+        id: 2,
+        nombre: 'Historia',
+        duracion: 6,
+        valor: 450000
+    },
+];
+
+
+const delay = (amount = number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, amount);
+    });
+}
+
+
+async function loop() {
+    for (let i = 0; i < cursos.length; i++) {
+        console.log("Curso " + i + " :", cursos[i]);
+        await delay(2000);
     }
-};
-let obtenerpromedio= (nota1, nota2, nota3) => (nota1+nota2+nota3)/3;
 
-console.log(obtenerpromedio(estudiante.notas.matematicas, estudiante.notas.ingles,estudiante.notas.programacion));
+    nombref='calculo';
+    let estudianteNota = cursos.find(function(nombreCurso ) {
+        return console.log(nombreCurso.nombre == nombref);
+    });
+}
 
-module.exports={
-    estudiante,
-    obtenerpromedio
-};
-console.log('test');
+
+console.log('Los cursos son los siguientes: ' + '\n' + 'Nota: Duracion del curso en meses');
+loop();
+
+
+const argv = require('yargs')
+    .command('promedio', 'Calcular el promedio', opciones)
+    .argv
+
+
+console.log(argv.matematicas + " 1");
+console.log( argv + " 2 ");
+console.log('El promedio es ' + (argv.m+argv.i+argv.p)/3);
