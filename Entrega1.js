93afa22eb1ceb8d9d5dd871b3d2c289555556dec
dacalles <dacalles@unal.edu.co>
2019-05-19 00:15:16 -0500

    Beta punto 2

--- Entrega1.js
+++ Entrega1.js
@@ -1,3 +1,6 @@
+const argv = require('yargs').argv;
+const fs = require('fs');
+
 let cursos = [{
     id: 0,
     nombre: 'calculo',
@@ -19,35 +22,50 @@ let cursos = [{
 ];
 
 
+nombreInteresado=argv.nombre;
+idInteresado=argv.id;
+cedulaInteresado=argv.cedula;
+inscribirInteresado=argv.inscribir;
+
 const delay = (amount = number) => {
     return new Promise((resolve) => {
         setTimeout(resolve, amount);
     });
 }
 
-
 async function loop() {
     for (let i = 0; i < cursos.length; i++) {
         console.log("Curso " + i + " :", cursos[i]);
-        await delay(2000);
+        await delay(1);
     }
-
-    nombref='calculo';
+    console.log('\n');
+    z=0;
     let estudianteNota = cursos.find(function(nombreCurso ) {
-        return console.log(nombreCurso.nombre == nombref);
+        //return console.log(nombreCurso.id == idInteresado);
+        z=z+1;
+        if ((nombreCurso.id == idInteresado)==true) {
+            z=z-1
+            console.log('El curso que selecciono es: ');
+            console.log(cursos[nombreCurso.id]);
+            crearArchivo(nombreInteresado, idInteresado, cedulaInteresado,cursos[nombreCurso.id] );
+
+        }else if (z==cursos.length){
+            console.log('El ID ingresado no corresponde a ningun curso en el programa, ingrese nuevamente un id valido');
+        }
     });
+
 }
 
-
+let crearArchivo = (nombreInteresado, idInteresado, cedulaInteresado, curso) =>{
+    texto= 'Usuario: ' + nombreInteresado + ' ' + ',con CC: ' + cedulaInteresado + ', ha buscado el curso con id: ' + idInteresado + '\n' + 'Los datos del curso que buscó son: ' +
+        'id: ' + curso.id + ', nombre: ' + curso.nombre + ', duracion: ' + curso.duracion + ', valor: ' + curso.valor;
+    fs.writeFile('Informacion.txt', texto, (err)=>{
+        if (err) throw (err);
+        console.log('se ha creado el archivo');
+    })
+};
 console.log('Los cursos son los siguientes: ' + '\n' + 'Nota: Duracion del curso en meses');
 loop();
 
 
-const argv = require('yargs')
-    .command('promedio', 'Calcular el promedio', opciones)
-    .argv
 
-
-console.log(argv.matematicas + " 1");
-console.log( argv + " 2 ");
-console.log('El promedio es ' + (argv.m+argv.i+argv.p)/3);
