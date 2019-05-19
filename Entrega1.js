f0bec7afe1ed1471277bf4e047a99aa2c68dd16a
dacalles <dacalles@unal.edu.co>
2019-05-18 20:39:36 -0500

    Initial commit

---
+++ Entrega1.js
@@ -0,0 +1,18 @@
+let estudiante = {
+    nombre: 'juan',
+    edad: 25,
+    notas: {
+        matematicas: 3,
+        ingles: 4,
+        programacion: 5
+    }
+};
+let obtenerpromedio= (nota1, nota2, nota3) => (nota1+nota2+nota3)/3;
+
+console.log(obtenerpromedio(estudiante.notas.matematicas, estudiante.notas.ingles,estudiante.notas.programacion));
+
+module.exports={
+    estudiante,
+    obtenerpromedio
+};
+console.log('test');
